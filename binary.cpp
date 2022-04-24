cc98cff7030c0cfcf28b39ba634056aefdfa4195
Suraj Kushwah <suraj@bugswriter.com>
2022-04-24 11:08:38 +0530

    s'

--- binary.cpp
+++ binary.cpp
@@ -1,42 +1,17 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-
-
-vector<int> k;
-
-
 int n = 4;
-
-void fun(int i){
-
-  if(i == n){
-    for(int j = 0; j < int(k.size()); j++){
-      cout << k[j];
-    }
-    cout << "\n";
+void fun(string k){
+  if(int(k.length()) == n){
+    cout << k << endl; 
   }
-
   else{
-
-    k.push_back(i);
-    fun(i+1);
-
-    k.pop_back();
-    fun(i+1);
-
-
-
+    fun(k+'0');
+    fun(k+'1');
   }
-
-
 }
-
 int main(){
-
-
-  fun(1);
-
+  fun("");
 }
 
 
